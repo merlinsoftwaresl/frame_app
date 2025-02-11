396846f5c5a5c1e34aabcb3e4b27914047cf6273
migueldeoleiros <miguel.lopez@merlinsoftware.es>
2025-02-11 10:34:04 +0100

    feat: initial commit with qr scanner

---
+++ android/app/src/main/kotlin/com/example/frame_app/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.frame_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+import 'barcode_scanner.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Frame App',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(),
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({super.key});
+  Widget _buildItem(BuildContext context, String label, Widget page) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.of(context).push(
+              MaterialPageRoute(
+                builder: (context) => page,
+              ),
+            );
+          },
+          child: Text(label),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Frame App')),
+      body: Center(
+        child: ListView(
+          children: [
+            _buildItem(
+              context,
+              'Scan QR to connect to frame',
+              const BarcodeScanner(),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
